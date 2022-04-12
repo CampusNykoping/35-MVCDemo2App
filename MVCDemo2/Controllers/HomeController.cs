c1930e8bc872d49d0f9d0c6a73fd024ba94c1e81
Claes Engelin <claes.engelin@zocom.se>
2022-04-12 10:56:20 +0200

    Add project files.

---
+++ MVCDemo2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MVCDemo2.Models;
+using MVCDemo2.Services;
+
+using System.Diagnostics;
+
+namespace MVCDemo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TotalUsers _totalUsers;
+
+        public HomeController(ILogger<HomeController> logger, TotalUsers totalUsers)
+        {
+            _logger = logger;
+            _totalUsers = totalUsers;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = _totalUsers.TUsers();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCDemo2/Services/TotalUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo2.Services;
+
+public class TotalUsers
+{
+    public long TUsers()
+    {
+        Random random = new Random();
+        return random.Next(10, 100);
+    }
+}
